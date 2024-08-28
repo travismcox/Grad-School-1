03ed548aadbff05260329d31897f9cb2b25c4fcb
Travis Cox <madman.in.a.blue.box@icloud.com>
2024-08-27 21:31:13 -0500

    Adding merge() and merge-sort without comments

--- 5501 Code/Untitled.py
+++ 5501 Code/Untitled.py
@@ -23,3 +23,32 @@ def insertion-sort(A):
             i = i - 1
         #Locating where to place key, insert at i+1, i being the location where either A[i] is less than key, or i = -1 in which case key is smallest element thus far
         A[i+1] = key
+        
+def merge(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    left_array = [0]*(n1+1)
+    right_array = [0]*(n2+1)
+    for i in range(n1):
+        left_array[i] = A[p + i - 1]
+    for j in range(n2):
+        right_array[j] = A[q + j]
+    left_array[n1] = float('inf')
+    right_array[n2] = float('inf')
+    i = 1
+    j = 1
+    for k in range(p,r):
+        if left_array[i] <= right_array[j]:
+            A[k] = left_array[i]
+            i = i + 1
+        else:
+            A[k] = right_array[j]
+            j = j + 1
+            
+def merge-sort(A, p, r):
+    if p < r:
+        q = (p + r) / 2
+        merge-sort(A, p, q)
+        merge-sort(A, q + 1, r)
+        merge(A, p, q, r)
+    
